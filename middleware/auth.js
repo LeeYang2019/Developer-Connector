d25d1f1f81afa66fa0dec4d0ae16281bf77bfb2a
Nhialee Yang <nhialeeyang@Nhialees-iMac.local>
2020-02-15 17:57:58 -0500

    updated actions

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,22 @@
-const jwt = require('jsonwebtoken')
-const config = require('config')
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 //exporting middleware function to get token
 module.exports = function(req, res, next) {
   //get token from header
-  const token = req.header('x-auth-token')
+  const token = req.header('x-auth-token');
 
   //check if token exists
   if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' })
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   //verify token if there is one, if matches else...
   try {
-    const decoded = jwt.verify(token, config.get('jwtToken'))
-    req.user = decoded.user
-    next()
+    const decoded = jwt.verify(token, config.get('jwtToken'));
+    req.user = decoded.user;
+    next();
   } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' })
+    res.status(401).json({ msg: 'Token is not valid' });
   }
-}
+};
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const connectDB = require('./config/db')
-const app = express()
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
 
 //connect database
-connectDB()
+connectDB();
 
 //init middleware
-app.use(express.json({ extended: false }))
+app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send(`API Running`))
+app.get('/', (req, res) => res.send(`API Running`));
 
 //Define Routes
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 5000 //if no environment set default to 5000
+const PORT = process.env.PORT || 5000; //if no environment set default to 5000
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
