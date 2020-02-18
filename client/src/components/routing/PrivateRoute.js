f41761ac4092ee134aa01b896c3f4970cbd93cd4
Nhialee Yang <nhialeeyang@Nhialees-iMac.local>
2020-02-18 11:40:01 -0500

    login complete

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
+    //check to see if user is not authenticated and not loading, if true redirect to login, else component will load
+    {...rest}
+    render={props =>
+      !isAuthenticated && !loading ? (
+        <Redirect to="/login" />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
