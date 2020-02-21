c8a091ee8bb8684ac6062ffe8e3c332f7667068d
Nhialee Yang <nhialeeyang@Nhialees-iMac.local>
2020-02-21 10:58:00 -0500

    Added Education

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({
   getCurrentProfile,
@@ -27,6 +29,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education experience={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
